39326ff906e5a0e3daf1c549bb17153358e69910
Ashutosh Kumar <akumar@Ashutoshs-MacBook-Air.local>
2025-10-15 22:03:54 +0530

    Completed the ActionHelperClass

--- src/main/java/utils/ActionHelper.java
+++ src/main/java/utils/ActionHelper.java
@@ -2,16 +2,14 @@ package utils;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.*;
 
-import java.awt.event.WindowFocusListener;
+import java.io.File;
 import java.time.Duration;
+import java.util.Set;
 
 public class ActionHelper {
 
@@ -28,6 +26,11 @@ public class ActionHelper {
         this.actions = new Actions(driver);
         this.js = (JavascriptExecutor) driver;
     }
+
+
+   /* *********************
+     Basic Operation
+    ************************/
 
     public void navigateTOUrl(String url){
         log.info("Navigating to URL: " +url);
@@ -92,4 +95,268 @@ public class ActionHelper {
         }
     }
 
+    public void jsClick(By locator){
+        try{
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            js.executeScript("arguments[0].click();", element);
+            log.info("Clicked via JS : {}",locator);
+        } catch (Exception e) {
+            log.error("JS click failed for element : {}",locator,e);
+        }
+    }
+
+    public void jsScrollToTop(){
+        try{
+            js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
+            log.info("Scrolled to top of page");
+        } catch (Exception e) {
+            log.error("Failed to scroll to top of the page");
+        }
+    }
+
+    /*****************
+    Dropdown Handling
+    ******************/
+
+
+    public void selectByVisibleText(By locator, String value){
+        try{
+            WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            new Select(dropdown).selectByVisibleText(value);
+            log.info("Selected '{}' from dropdown: {} ",value,locator);
+        } catch (Exception e) {
+            log.error("Failed to select dropdown by visible text to element: " +locator,e);
+            throw e;
+        }
+    }
+
+    public void selectByValue(By locator, String value){
+        try{
+            WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            new Select(dropdown).selectByValue(value);
+            log.info("Selected '{}' from dropdown: {} ",value,locator);
+        } catch (Exception e) {
+            log.error("Failed to select dropdown by value to element: " +locator,e);
+            throw e;
+        }
+    }
+
+    public void selectByIndex(By locator, int index){
+        try{
+            WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            new Select(dropdown).selectByIndex(index);
+            log.info("Selected '{}' from dropdown: {} ",index,locator);
+        } catch (Exception e) {
+            log.error("Failed to select dropdown by visible text to element: " +locator,e);
+            throw e;
+        }
+    }
+
+    /********************
+    Advance Mouse Actions
+     *********************/
+
+    public void hoverElement(By locator){
+      try{
+          WebElement element =  wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+          actions.moveToElement(element).perform();
+          log.info("Hovered on element: {}", locator);
+      }catch (Exception e){
+          log.error("Failed to Hover to element: " +locator,e);
+          throw e;
+      }
+    }
+
+    public void hoverElementAndClick(By locator){
+        try{
+            WebElement element =  wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            actions.moveToElement(element).click().build().perform();
+            log.info("Hovered on element and click on: {}", locator);
+        }catch (Exception e){
+            log.error("Failed to Hover to element and click: " +locator,e);
+            throw e;
+        }
+    }
+
+    public void doubleClick(By locator){
+        try{
+            WebElement element =  wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            actions.doubleClick(element).perform();
+            log.info("Double clicked on element: {}", locator);
+        }catch (Exception e){
+            log.error("Failed to Double Click to element: " +locator,e);
+            throw e;
+        }
+    }
+
+    public void rightClick(By locator){
+        try{
+            WebElement element =  wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            actions.contextClick(element).perform();
+            log.info("Right clicked on element: {}", locator);
+        }catch (Exception e){
+            log.error("Failed to Right Clicked to element: " +locator,e);
+            throw e;
+        }
+    }
+
+    public void dragAndDrop(By source, By target){
+        try{
+           WebElement src =  wait.until(ExpectedConditions.visibilityOfElementLocated(source));
+           WebElement tgt =  wait.until(ExpectedConditions.visibilityOfElementLocated(target));
+           actions.dragAndDrop(src,tgt).perform();
+           log.info("Drageed element {} to {} ", source,target);
+        }catch (Exception e){
+            log.error("Failed to DragAndDrop from source {} to target {}: ",source,target,e);
+            throw e;
+        }
+    }
+
+
+    /**************
+ *  Alert Handling
+ *  ****************/
+
+
+    public void acceptAlert(){
+        try{
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            log.info("Accepting alert: {}",alert.getText());
+            alert.accept();
+        }catch (Exception e){
+            log.warn("No alert to accept");
+        }
+    }
+
+    public void dismissAlert(){
+        try{
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            log.info("Dismissing alert: {}",alert.getText());
+            alert.dismiss();
+        }catch (Exception e){
+            log.warn("No alert to dismiss");
+        }
+    }
+
+    public String getAlertText(){
+        try{
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            log.info("Alert text : {}",alert.getText());
+            return alert.getText();
+        }catch (Exception e){
+            log.warn("No alert present");
+            return null;
+        }
+        }
+
+
+    /*******************
+     Windows/Tab Actions
+     ******************** */
+
+
+    public void switchTOWindowByTitle(String title) {
+        try {
+            Set<String> handles = driver.getWindowHandles();
+            for (String handle : handles) {
+                driver.switchTo().window(handle);
+                if (driver.getTitle().contains(title)) {
+                    log.info("Switched to window: {}", title);
+                    return;
+                }
+            }
+        } catch (Exception ex) {
+           log.warn("No window found with title");
+        }
+    }
+
+    public void switchToNewTab() {
+        try {
+           String current = driver.getWindowHandle();
+            for (String handle : driver.getWindowHandles()) {
+               if (!handle.equals(current)){
+                   driver.switchTo().window(handle);
+                   log.info("Switching to new tab");
+                   return;
+               }
+            }
+        } catch (Exception ex) {
+            log.warn("Failed to switch to new Tab");
+        }
+    }
+
+   /* Wait Utils*/
+
+    public void waitForVisibility(By locator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void waitForInvisibility(By locator){
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    public void fluentWait(By locator, int timeoutSeconds){
+        Wait<WebDriver> fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(timeoutSeconds))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class);
+        fluentWait.until(driver -> driver.findElement(locator));
+        log.info("Fluent wait complete for element: {} ",locator);
+    }
+
+    /*****************
+    Screenshot Utility
+    *******************/
+    public String takeScreenshot(String fileName){
+        try{
+            File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            String dest = System.getProperty("user,dir")+ "/screenshots/"+ fileName + ".png";
+            FileHandler.createDir(new File("screenshots"));
+            FileHandler.copy(src,new File(dest));
+            log.info("Screenshot captured: {} ",dest);
+            return dest;
+        } catch (Exception e) {
+           log.error("Screenshot capture failed",e);
+           return null;
+        }
+    }
+
+    public void pressEnter(By locator){
+        driver.findElement(locator).sendKeys(Keys.ENTER);
+        log.info("Pressed ENTER on element: {}",locator);
+    }
+
+    public void clearText(By locator){
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.clear();
+        log.info("Cleared text for element: {}", locator);
+    }
+
+    public boolean verifyTextEquals(By locator, String expectedText){
+        String actual = getText(locator);
+        boolean match = actual.trim().equalsIgnoreCase(expectedText.trim());
+        log.info("Verifying text match | Expected : {} | Actual: {}",expectedText,actual);
+        return match;
+    }
+
+    public void uploadFile(By locator, String filePath){
+       WebElement element =  wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+       element.sendKeys(filePath);
+       log.info("Uploaded file: {} ",filePath);
+    }
+
+    public void refreshPage(){
+        driver.navigate().refresh();
+        log.info("Page refreshed");
+    }
+
+    public void back(){
+        driver.navigate().back();
+        log.info("Navigated Back");
+    }
+
+    public void forward(){
+        driver.navigate().forward();
+        log.info("Navigated Forward");
+    }
 }
